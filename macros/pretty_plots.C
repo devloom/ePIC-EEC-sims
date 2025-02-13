03bf4096ac27ff248dbeeee1aecc04ec3ff73ed5
Shaun Gu <miyagu@umich.edu>
2025-02-12 19:32:20 -0500

    I have updated the run_eec and pretty_plots, which should work now but still need many refinement.

---
+++ macros/pretty_plots.C
@@ -0,0 +1,146 @@
+#include "../include/HistoManager.h"
+
+
+R__LOAD_LIBRARY(../lib/libEnergyCorrelator.so)
+
+void pretty_plots()
+{
+    TFile *file_truth = TFile::Open("test_outfile_truth.root", "READ");
+    TFile *file_smeared = TFile::Open("test_outfile_smeared.root", "READ");
+    
+    TH1F *jPt_truth = (TH1F*)file_truth->Get("jetPt");
+    TH1F *jPt_smeared = (TH1F*)file_smeared->Get("jetPt");
+   
+
+	TCanvas* c1 = new TCanvas("c1", "Histogram Canvas", 1600, 1000); // 800 and 600 tell the program the size of the histogram
+	c1->Divide(4,2);
+    double xmax = 1;
+	double xmin =0.0005;
+	int bins_low=100;
+	int bins_high=10; 
+	double binwidth_low=(xmax-xmin)/bins_low;
+	double binwidth_high=(xmax-xmin)/bins_high;
+    int pTarray[7]={5,10,15,20,25,30,35};
+
+    double jetCounts_truth[7] = {0}, jetCounts_smeared[7] = {0};
+
+    for (int i = 0; i < 6; ++i) {  
+		int low=jPt_truth->FindBin(pTarray[i]);
+		int high=jPt_truth->FindBin(pTarray[i+1])-1;
+
+		for (int j=low; j<= high; ++j)
+			{jetCounts_truth[i]+=jPt_truth->GetBinContent(j);}}
+				
+
+	for (int i = 0; i < 6; ++i) {  
+		int low=jPt_smeared->FindBin(pTarray[i]);
+		int high=jPt_smeared->FindBin(pTarray[i+1])-1;
+
+		for (int j=low; j<= high; ++j)
+			{jetCounts_smeared[i]+=jPt_smeared->GetBinContent(j);}}
+
+	
+		
+
+
+
+    std::vector<TH1F*> EEC_hist_smeared(6), EEC_hist_truth(6);
+
+    for (int i = 0; i < 6; ++i) {                                      //get the histogram from root outfile
+        std::string hist_name_smeared = Form("eec_pt_bin_%d_%d", (int)pTarray[i], (int)pTarray[i+1]);
+        std::string hist_name_truth = Form("eec_pt_bin_%d_%d", (int)pTarray[i], (int)pTarray[i+1]);
+
+        EEC_hist_smeared[i] = (TH1F*)file_smeared->Get(hist_name_smeared.c_str());
+        EEC_hist_truth[i] = (TH1F*)file_truth->Get(hist_name_truth.c_str());
+
+        if (!EEC_hist_smeared[i] || !EEC_hist_truth[i]) {
+            std::cerr << "Error: Missing EEC histogram for bin " << i << std::endl;
+            return;
+        }
+    }
+
+
+
+	for (int i=0; i<6; ++i){
+		c1->cd(i+1);
+		gPad->SetLeftMargin(0.15);
+		double binwidth;
+        if (i<4){binwidth=binwidth_low;}
+		if (i>3){binwidth=binwidth_high;}
+        EEC_hist_smeared[i]->Scale(1. / (jetCounts_smeared[i]*binwidth));
+		EEC_hist_truth[i]->Scale(1. / (jetCounts_truth[i]*binwidth));
+
+
+
+		EEC_hist_smeared[i]->GetXaxis()->SetRangeUser(0.0005, 10);
+		EEC_hist_truth[i]->GetXaxis()->SetRangeUser(0.0005, 10);
+
+		EEC_hist_smeared[i]->SetMaximum(10.); // Set y-axis maximum
+    	EEC_hist_smeared[i]->SetMinimum(0.001); // Set y-axis maximum
+		EEC_hist_truth[i]->SetMaximum(10.); // Set y-axis maximum
+    	EEC_hist_truth[i]->SetMinimum(0.001); // Set y-axis maximum
+
+		EEC_hist_smeared[i]->SetLineColor(kRed); 
+        EEC_hist_truth[i]->SetLineColor(kGreen); 
+
+		EEC_hist_smeared[i]->Draw("Pe"); // Draw the histogram
+		EEC_hist_truth[i]->Draw("Pe SAME"); // Draw the histogram
+		
+		EEC_hist_smeared[i]->GetXaxis()->SetTitle("x_{L}");
+		EEC_hist_smeared[i]->GetYaxis()->SetTitle("#frac{1}{N_{jet}} #frac{d #sigma^{[2]}}{dx_{L}}");
+		EEC_hist_truth[i]->GetXaxis()->SetTitle("x_{L}");
+		EEC_hist_truth[i]->GetYaxis()->SetTitle("#frac{1}{N_{jet}} #frac{d #sigma^{[2]}}{dx_{L}}");
+		
+		std::cout << "Histogram: " << EEC_hist_smeared[i]->GetName() 
+          << ", Xmin: " << EEC_hist_smeared[i]->GetXaxis()->GetXmin() 
+          << ", Xmax: " << EEC_hist_smeared[i]->GetXaxis()->GetXmax() 
+          << std::endl;
+
+
+		TRatioPlot* rp = new TRatioPlot(EEC_hist_truth[i], EEC_hist_smeared[i]);
+        rp->SetH1DrawOpt("PE");  // so the top pad is drawn with points
+        rp->SetH2DrawOpt("PE");  // so the ratio is drawn with points
+        rp->Draw("SAME"); 
+
+		rp->GetUpperPad()->SetLogx(true);
+		rp->GetUpperPad()->SetLogy(true);
+		rp->GetLowerPad()->SetLogx(true);
+		rp->GetLowerPad()->SetLogy(false);
+        rp->GetLowerRefGraph()->GetYaxis()->SetRangeUser(0, 6);
+		rp->GetLowerRefGraph()->GetYaxis()->SetTitle("ratio");
+
+		// EEC_hist_smeared[i]->SetTitle("2-point EEC"); // Title
+
+
+		// gPad->Update();
+		gPad->SetLogx();
+		gPad->SetLogy();
+
+		TLatex latex;
+    	latex.SetNDC();
+    	latex.SetTextSize(0.03);
+
+        TLegend *legend = new TLegend(0.25, 0.25, 0.47, 0.35);  // Position the legend
+		legend->SetBorderSize(0); 
+		legend->SetFillStyle(0);
+		legend->SetTextSize(0.03);
+        legend->AddEntry(EEC_hist_smeared[i], "smeared", "l");
+        legend->AddEntry(EEC_hist_truth[i], "truth", "l");
+		gPad->Update();
+		}
+
+	
+	c1->cd(7);
+	TLatex latex;
+	latex.SetNDC();
+    latex.SetTextSize(0.03);
+	latex.DrawLatex(0.5, 0.85, "1000000 events");
+    latex.DrawLatex(0.5, 0.80, "pT hat cut = 5 GeV");
+	latex.DrawLatex(0.5, 0.75, "Eta cut = +-3.5GeV");
+	latex.DrawLatex(0.5, 0.70, "jet radius= 1.0");
+
+
+    c1->Update();
+	c1->SaveAs("../../simulations/plots/E2C_pretty_plot.pdf"); // Save the histogram as a PDF file
+
+}
--- macros/run_eec.C
+++ macros/run_eec.C
@@ -9,7 +9,7 @@ void run_eec()
     // ========== For now just read in the vector branches and we will manually make the jet objects ========== //
     // read pythia jets from file
     // std::string inputPythia = "pythia8_inclusivejets.root";
-    std::string inputPythia = "../../simulations/truth_jets.root";
+    std::string inputPythia = "../../simulations/smeared_p_excl_jets.root";
     TFile *infilePythia = new TFile(inputPythia.c_str());
     if (!infilePythia || infilePythia->IsZombie()){std::cerr << "Could not open file." << std::endl;return;}
 
@@ -19,29 +19,42 @@ void run_eec()
     if (!jettree){std::cerr << "Could not find tree." << std::endl;infilePythia->Close();return;}
 
     int nConstituents;
-	double jPx, jPy, jPz, jE;
+	  double jPx, jPy, jPz, jE, jPt;
     std::vector<int>* dtr_PID = nullptr;
     std::vector<double> *dtr_Px = nullptr, *dtr_Py = nullptr, *dtr_Pz = nullptr,  *dtr_E = nullptr;
-	jettree->SetBranchAddress("nConstituents", &nConstituents); 
-	jettree->SetBranchAddress("dtr_Px" , &dtr_Px);
-	jettree->SetBranchAddress("dtr_Py" , &dtr_Py);
-	jettree->SetBranchAddress("dtr_Pz" , &dtr_Pz);
-	jettree->SetBranchAddress("dtr_E" , &dtr_E);
-	jettree->SetBranchAddress("dtr_PID" , &dtr_PID);
+	  jettree->SetBranchAddress("nConstituents", &nConstituents); 
+	  jettree->SetBranchAddress("dtr_Px" , &dtr_Px);
+	  jettree->SetBranchAddress("dtr_Py" , &dtr_Py);
+	  jettree->SetBranchAddress("dtr_Pz" , &dtr_Pz);
+	  jettree->SetBranchAddress("dtr_E" , &dtr_E);
+	  jettree->SetBranchAddress("dtr_PID" , &dtr_PID);
+    jettree->SetBranchAddress("jPt" , &jPt);
     // ================================================= //
 
     // Create histograms
     std::vector<float> ptbins = {5,10,15,20,25,30,35,40};
     HistoManager::InitializeHistograms(ptbins);
+    TH1F *jetPt = new TH1F("jetPt", "Jet pT Distribution; pT (GeV); Number of Jets", 40, 0, 200);  // 40 bins, 0 to 200 GeV
 
     for (int i = 0; i < jettree->GetEntries(); ++i)
+    //for (int i = 0; i < jettree->GetEntries(); ++i)
     {
       if (i % 100000 == 0){std::cout << "Processed " << i << " jets." << std::endl;}
       jettree->GetEntry(i);
-      if (nConstituents < 3){continue;}
+      
+      int count=0;
+      for (int i = 0; i < nConstituents; i++)
+						{if (dtr_PID->at(i) == 0) {
+							count++;}}
+					if (count==nConstituents)
+						{continue;}
+      //std::cout << nConstituents << std::endl;
+      jetPt->Fill(jPt);  //store jPt for jet count for scaling
+
       Jet *jet = new Jet();
       for (int j = 0; j < nConstituents; j++)
       {
+        
         Particle *part = new Particle((*dtr_PID)[j],(*dtr_Px)[j],(*dtr_Py)[j],(*dtr_Pz)[j],(*dtr_E)[j]);
         jet->addConstituent(*part);
       }
@@ -49,8 +62,9 @@ void run_eec()
       eec->FillHistograms();
     }
 
-    TFile *outfile = new TFile("test_outfile.root","RECREATE");
+    TFile *outfile = new TFile("test_outfile_smeared.root","RECREATE");
     HistoManager::WriteHistogramsToFile();
+    jetPt->Write(); 
     outfile->Close();
 
     HistoManager::Cleanup();
