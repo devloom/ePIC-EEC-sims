a9a662d6d7b478f6ef7636fc0da341255a6f7d26
EIC Container Build Service <gitlab@eicweb.phy.anl.gov>
2025-03-13 16:09:03 -0400

    add updated functionality to src code

--- src/HistoManager.cpp
+++ src/HistoManager.cpp
@@ -9,6 +9,7 @@ namespace HistoManager {
   {
     int bins[7]={200,100,75,50,30,6,35};
     ptBinEdges = edges;
+    histograms["jetCounts"] = new TH1I("jetCounts","jetCounts",ptBinEdges.size(),0,ptBinEdges.size());
     histograms["weight"] = new TH1D("weight", "weight", 100, 0, 0.25);
     histograms["weight_E3C"] = new TH1D("weight_E3C", "weight_E3C", 100, 0, 0.25);
     histograms["deltaR"] = new TH1D("deltaR", "DeltaR", 100, 0, 1);
@@ -22,9 +23,6 @@ namespace HistoManager {
       histName << "eec_pt_bin_" << ptBinEdges[i] << "_" << ptBinEdges[i + 1];
       histograms[histName.str()] = new TH1D(histName.str().c_str(), histName.str().c_str(), bins[i], 0.0005, 1);
 
-      // std::ostringstream histName_E3C;
-      // histName_E3C << "E3C_pt_bin_" << ptBinEdges[i] << "_" << ptBinEdges[i + 1];
-      // histograms[histName_E3C.str()] = new TH1D(histName_E3C.str().c_str(), histName_E3C.str().c_str(), bins[i], 0.0005, 1);
       std::ostringstream histName_E3C;
       histName_E3C << "E3C_pt_bin_" << static_cast<int>(ptBinEdges[i]) << "_" << static_cast<int>(ptBinEdges[i + 1]);
       histograms[histName_E3C.str()] = new TH1D(histName_E3C.str().c_str(), histName_E3C.str().c_str(), bins[i], 0.0005, 1);
